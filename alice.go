308cf4a32a091588613fc88a81f14639834a2a07
andrewarderne <andrew.arderne@navenio.com>
2018-12-23 21:29:28 +0000

    finished simple diffie hellman over http

--- alice.go
+++ alice.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"math"
 	"net/http"
 	"reflect"
 	"strconv"
@@ -17,9 +18,9 @@ func main() {
 	publicG = 5
 	publicP = 23
 	privateAlice = 4
-	// aliceInter := math.Mod(math.Pow(publicG, privateAlice), publicP)
-	// Send info over to bob
-	resp, err := http.Get("http://localhost:8001/gen")
+	aliceInter := int(math.Mod(math.Pow(publicG, privateAlice), publicP))
+	url := fmt.Sprintf("%s%d", "http://localhost:8001/gen?aliceInter=", aliceInter)
+	resp, err := http.Get(url)
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -27,16 +28,12 @@ func main() {
 	if err != nil {
 		log.Fatalln(err)
 	}
-	temp := reflect.TypeOf(body).Kind()
-	fmt.Println(temp)
-	x := string(body[:])
-	fmt.Println(x)
-	num, _ := strconv.ParseInt(x, 10, 64)
-	fmt.Println("ment to be int", num)
-	fmt.Println(reflect.TypeOf(x))
-	log.Println(string(body))
-	// bobIntermediateKey := request.get("url_to_bob", aliceInter)
-	// key := math.Mod(math.Pow(bobIntermediateKey, privateAlice), publicP)
+	bobInter, _ := strconv.ParseFloat(string(body), 64)
+	fmt.Println("ment to be int", bobInter)
+	fmt.Println(reflect.TypeOf(bobInter))
+
+	key := math.Mod(math.Pow(bobInter, privateAlice), publicP)
+	fmt.Println("Alices private key is:", key)
 	// http.HandleFunc("/", setup_conn)
 	// http.ListenAndServe(":8000", nil)
 }
--- bob.go
+++ bob.go
@@ -2,22 +2,29 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"net/http"
+	"strconv"
 )
 
-// var publicG float64
-// var publicP float64
+var publicG float64
+var publicP float64
+var privatebBob float64
 
 func KeyGen(w http.ResponseWriter, r *http.Request) {
-	// privatebBob = 3
-	// r.ParseForm()
-	// publicG := r.FormValue("publicG")
-	// publicP := r.FormValue("publicP")
-	// aliceInter := r.FormValue("aliceInter")
-	// key := math.Mod(math.Pow(aliceInter, privatebBob), publicP)
-	// fmt.Println(key)
-	// bobInter := math.Mod(math.Pow(publicG, privatebBob), publicP)
-	fmt.Fprint(w, 10)
+	privatebBob = 3
+	publicG = 5
+	publicP = 23
+	r.ParseForm()
+	input := r.FormValue("aliceInter")
+	aliceInter, _ := strconv.ParseFloat(input, 64)
+
+	// fmt.Println(reflect.TypeOf(aliceInter))
+
+	key := math.Mod(math.Pow(aliceInter, privatebBob), publicP)
+	bobInter := math.Mod(math.Pow(publicG, privatebBob), publicP)
+	fmt.Println("Bobs private key is: ", key)
+	fmt.Fprint(w, bobInter)
 }
 
 func main() {
