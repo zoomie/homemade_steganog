b9c9c4239b8ff3c1de6707fe22b73f67fc5bc8cb
Andrew Arderne <andrew.arderne@navenio.com>
2019-02-04 00:01:29 +0000

    indexs are now a generator

--- homemade_steganog/main.py
+++ homemade_steganog/main.py
@@ -8,101 +8,111 @@ class Steg:
     image. 
     """
     def __init__(self):
-        self.settings = dict()
         self.letters =\
         """0123456789abcdefghijklmnopqrstuvwxyz ,.:-'"?;()!"""
-        self.normal_img = None
+        self.input_img = None
         self.encode_data = None
-        self.flat_img = None
-        self.encode_indexs = None
         self.data_img = None
         self.char_num = None
 
-    
     def load_img(self, img_path):
         """
         Input the path to the image file
         """
-        self.normal_img = imread(img_path)
+        self.input_img = imread(img_path)
 
-    def load_data(self, data_path):
+    def load_data(self, data_path, ending_numbers=None):
         """
-        Input path to the txt file
+        Input path to the txt file.
+        Add the number of digits to the
+        start of the data.
         """
         with open(data_path, 'r') as f:
             data = f.read().lower()
-        self.encode_data = self._char_to_num(data)
-
-    def encrypt(self):
-        """
-        The encrypt methods encoded the data into
-        the image by changing pixels value, the index 
-        is chosen base on the random number generator.
-        """
-        self.encode_indexs = self._create_indexs(self.normal_img, 
-                                                 self.encode_data)
-        self.data_img = self._input_data(self.normal_img, 
-                                         self.encode_indexs, 
-                                         self.encode_data)
-        return self.data_img
+        data_in_nums = self._char_to_num(data)
+        if not ending_numbers:
+            ending_numbers = [0, 99]
+        self.encode_data = data_in_nums + ending_numbers
 
     def _char_to_num(self, chars):
         nums = list()
         char_num = {char: num for num, char in enumerate(self.letters)}
         char_num['\n'] = int(len(self.letters)+1)
         char_num['\t'] = int(len(self.letters)+2)
-        self.char_num = char_num
         for char in chars:
             num = char_num[char]
             nums.append(num)
         return nums
 
-    def _create_indexs(self, img, data):
+    def encrypt(self, num_generator=None):
+        """
+        The encrypt methods encoded the data into
+        the image by changing pixels value, the index 
+        is chosen base on the random number generator.
+        The encryption and decryption methods must both
+        use the same num_generator.
+        """
+        index_gen = self._yield_indexs(self.input_img, 
+                                       num_generator)
+        data_in_img = self._input_data(self.input_img, 
+                                       self.encode_data, 
+                                       index_gen)
+        return data_in_img
+
+    def _yield_indexs(self, img, num_generator=None):
+        if not num_generator:
+            num_generator = self.default_num_generator()
         indexs = list()
         position = 0
         num = 0
-        generator = self._num_generator(img, data)
         xs, ys, zs = img.shape
         for x in range(xs):
             for y in range(ys):
                 for z in range(zs):
                     if position == num:
-                        indexs.append((x, y, z))
+                        yield x, y, z
                         position = 0
-                        num = next(generator)
+                        num = next(num_generator)
                     position += 1
-                    if len(indexs) == len(data):
-                        return indexs  
 
-
-    def _input_data(self, img, encode_indexs, data):
-        for num, (x, y, z) in enumerate(encode_indexs):
-            img[x][y][z] = data[num]
+    def _input_data(self, img, data, index_gen):
+        # zip loops to the lenght of the smallest 
+        # iterable, therefore it will stop at the 
+        # end of data
+        for digit, (x, y, z) in zip(data, index_gen):
+            img[x][y][z] = digit
         return img
 
-    """
-    The following generator specifies where the 
-    index of the encoded data.
-    """
-    def _num_generator(self, img, data):
-        x, y, z = img.shape
-        space_between_data = int((x*y*z)/len(data))
-        largest_space = space_between_data + 10
-        smallest_space = space_between_data - 40
-        while True:
-        #     yield 50
-            yield randint(smallest_space, largest_space)
-
-    def decrypt_img(self, img):
+    def decrypt_img(self, img, num_generator=None):
+        """
+        Extracts text from the image.
+        """
+        if not num_generator:
+            num_generator = self.default_num_generator()
+        index_gen = self._yield_indexs(img, num_generator)
         data = list()
-        for x, y, z in self.encode_indexs:
+        for i, (x, y, z) in enumerate(index_gen):
             data.append(img[x, y, z])
+            if i > 10 and data[i] == 99 and data[i-1] == 0:
+                # Remove the ending values
+                data = data[:-2]
+                break
         message = self._num_to_char(data)
         return ''.join(message)
     
     def _num_to_char(self, nums):
         chars = list()
-        num_char = {num: char for char, num in self.char_num.items()}
+        num_char = {num: char for num, char in enumerate(self.letters)}
+        num_char[int(len(self.letters)+1)] = '\n'
+        num_char[int(len(self.letters)+2)] = '\t'
         for num in nums:
             chars.append(num_char[num])
         return chars
+
+    def default_num_generator(self):
+        """
+        The following generator specifies where the 
+        index of the encoded data.
+        """
+        while True:
+            yield 184
