1b5f683b8e43b98d47e5186fc86cc78ad97cd2a0
Andrew Arderne <andrew.arderne@navenio.com>
2019-02-04 23:37:39 +0000

    updated the version on pipy

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='homemade_steganog',
-    version='0.1.3',
+    version='0.2.0',
     packages=['homemade_steganog',],
     install_requires=['scikit-image'],
     license='MIT',
